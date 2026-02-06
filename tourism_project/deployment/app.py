1c017297d9c725948bf72c9d5318750e27cee8c1
manju30-code <manjukmoorthi@gmail.com>
2026-02-06 16:18:28 +0000

    first commit

---
+++ tourism_project/deployment/app.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import pandas as pd
+from huggingface_hub import hf_hub_download
+import joblib
+
+# Download and load the model
+model_path = hf_hub_download(repo_id="mkrish2025/Tourism-Customer-Prediction", filename="best_tourism_prediction_model_v1.joblib")
+model = joblib.load(model_path)
+
+# Streamlit UI for Machine Failure Prediction
+st.title("Customer Tour Package Prediction App")
+st.write("""
+This application predicts the likelihood of customers purchasing the Wellness Tourism Package.
+Please enter the customer details below to get a prediction.
+""")
+
+Age = st.number_input("Age", min_value=18, max_value=100, value=30)
+MonthlyIncome = st.number_input("Monthly Income", min_value=1000, max_value=100000, value=5000)
+TypeofContact = st.selectbox("Type of Contact", ["Self Enquiry", "Company Invited"])
+CityTier = st.number_input("City Tier", min_value=1, max_value=3, value=1)
+Occupation = st.selectbox("Occupation", ["Salaried", "Small Business", "Large Business"])
+Gender = st.selectbox("Gender", ["Male", "Female"])
+ProductPitched = st.selectbox("Product Pitched", ["Basic", "Deluxe", "Standard", "Super Deluxe", "King"])
+MaritalStatus = st.selectbox("Marital Status", ["Single", "Married", "Unmarried", "Divorced"])
+Passport = st.number_input("Passport (0=No, 1=Yes)", min_value=0, max_value=1, value=0, step=1)
+OwnCar = st.number_input("Own Car (0=No, 1=Yes)", min_value=0, max_value=1, value=0, step=1)
+Designation = st.selectbox("Designation", ["Executive", "Manager", "Senior Manager", "AVP", "VP"])
+PreferredPropertyStar = st.number_input("Preferred PropertyStar", min_value=3, max_value=5, value=3, step=1)
+NumberOfTrips = st.number_input("Number Of Trips", min_value=0, max_value=100, value=2, step=1)
+TotalVisiting = st.number_input("Total Visting (includes child below 5)", min_value=1, max_value=10, value=2, step=1)
+
+
+# Assemble input into DataFrame
+input_data = pd.DataFrame([{
+    "Age": Age,
+    "MonthlyIncome": MonthlyIncome,
+    "Designation": Designation,
+    "OwnCar": OwnCar,
+    "Passport": Passport,
+    "CityTier": CityTier,
+    "MaritalStatus": MaritalStatus,
+    "ProductPitched": ProductPitched,
+    "Gender": Gender,
+    "Occupation": Occupation,
+    "TypeofContact": TypeofContact,
+	  "PreferredPropertyStar": PreferredPropertyStar,
+	  "NumberOfTrips": NumberOfTrips,
+	  "TotalVisiting": TotalVisiting
+	
+}])
+
+st.subheader("Raw Input Data")
+st.dataframe(input_data)
+
+# Prediction 	
+if st.button("Predict"):
+    try:
+        # Apply same preprocessing as training
+        X_processed = preprocessor.transform(input_data)
+
+        prediction = model.predict(X_processed)[0]
+        prediction_proba = model.predict_proba(X_processed)[0][1]
+
+        if prediction == 1:
+            st.success(f"✅ Customer is likely to take the product (Confidence: {prediction_proba:.2f})")
+        else:
+            st.warning(f"❌ Customer is unlikely to take the product (Confidence: {1 - prediction_proba:.2f})")
+
+    except Exception as e:
+        st.error(f"Prediction failed: {e}")	
+	
+	
---
+++ tourism_project/model_building/prep.py
@@ -0,0 +1,89 @@
+# for data manipulation
+import pandas as pd
+import sklearn
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.pipeline import Pipeline
+from huggingface_hub import HfApi
+
+# Define constants for the dataset and output paths
+api = HfApi(token=os.getenv("HF_TOKEN"))
+DATASET_PATH = "hf://datasets/mkrish2025/Tourism-Customer-Prediction/tourism.csv"
+df = pd.read_csv(DATASET_PATH)
+print("Dataset loaded successfully.")
+
+# Drop the unique identifier
+df.drop(df.columns[0], axis=1,inplace=True) #running number of rows
+df.drop(columns=['CustomerID'], inplace=True)
+
+# Correcting data gaps
+df['Gender'] = df['Gender'].apply(lambda x: 'Female' if x == 'Fe Male' else x)
+df['Occupation'] = df['Occupation'].apply(lambda x: 'Salaried' if x == 'Free Lancer' else x)
+
+# Combining similar representation as Total no of visitors.
+df['TotalVisiting'] = df['NumberOfPersonVisiting'] + df['NumberOfChildrenVisiting']
+df.drop(columns=['NumberOfPersonVisiting', 'NumberOfChildrenVisiting'], axis=1, inplace=True)
+
+# Customer Interaction Data will not be required for prediction as this is updated post interaction.
+# Will retain 'ProductPitched' alone as this will be required for pitching the product
+df.drop(columns=['PitchSatisfactionScore', 'NumberOfFollowups', 'DurationOfPitch'], axis=1, inplace=True)
+
+target_col = 'ProdTaken'
+
+# Split into X (features) and y (target)
+X = df.drop(columns=[target_col])
+y = df[target_col]
+
+# Perform train-test split
+Xtrain, Xtest, ytrain, ytest = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+numeric_features = ['Age','MonthlyIncome']
+categorical_features = ['Designation', 'OwnCar', 'Passport',
+            'CityTier','MaritalStatus',
+            'ProductPitched','Gender','Occupation','TypeofContact'
+            ]
+
+# Preprocessing pipeline
+preprocessor = make_column_transformer(
+    (StandardScaler(), numeric_features),
+    (OneHotEncoder(handle_unknown='ignore'), categorical_features),
+    remainder="passthrough"
+)
+
+# Fit ONLY on training data
+Xtrain_processed = preprocessor.fit_transform(Xtrain)
+Xtest_processed = preprocessor.transform(Xtest)
+
+
+feature_names = preprocessor.get_feature_names_out()
+
+Xtrain_df = pd.DataFrame(
+    Xtrain_processed.toarray(),
+    columns=feature_names
+)
+
+Xtest_df = pd.DataFrame(
+    Xtest_processed.toarray(),
+    columns=feature_names
+)
+
+
+Xtrain.to_csv("Xtrain.csv",index=False)
+Xtest.to_csv("Xtest.csv",index=False)
+ytrain.to_csv("ytrain.csv",index=False)
+ytest.to_csv("ytest.csv",index=False)
+
+
+files = ["Xtrain.csv","Xtest.csv","ytrain.csv","ytest.csv"]
+
+for file_path in files:
+    api.upload_file(
+        path_or_fileobj=file_path,
+        path_in_repo=file_path.split("/")[-1],  # just the filename
+        repo_id="mkrish2025/Tourism-Customer-Prediction",
+        repo_type="dataset",
+    )
